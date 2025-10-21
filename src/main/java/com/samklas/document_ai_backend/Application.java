e1943701ba546a6a2f47c63ce64763f55d8ddb3c
Samuel Klas <klassam95@gmail.com>
2025-10-21 23:15:12 +0300

    init

---
+++ src/main/java/com/samklas/document_ai_backend/Application.java
@@ -0,0 +1,13 @@
+package com.samklas.document_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/samklas/document_ai_backend/DocumentAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samklas.document_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
