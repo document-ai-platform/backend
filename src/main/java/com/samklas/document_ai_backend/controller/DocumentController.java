bf8ed335730ff27ee3f995d8269cbae3f165803d
Samuel Klas <klassam95@gmail.com>
2025-11-02 22:47:52 +0200

    sallittu cross origin

--- src/main/java/com/samklas/document_ai_backend/controller/DocumentController.java
+++ src/main/java/com/samklas/document_ai_backend/controller/DocumentController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/documents")
+@CrossOrigin(origins = "*")
 public class DocumentController {
 
     private final DocumentService documentService;
